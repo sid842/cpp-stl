76a88659112637217ee548a013ea262b3addee5f
sid <sidbhasin82@gmail.com>
2019-10-01 23:38:19 +0530

    added code for map

--- associative-containers.cpp
+++ associative-containers.cpp
@@ -11,6 +11,18 @@
 
 using namespace std;
 
+void foo(const map<int, string>& m) {
+	//Below line doesn't work since operator[] tries to modify the map 
+	//cout << m[3] << " ";
+	map<int, string>::const_iterator itr = m.find(3);	//could have used auto here
+
+	if(itr == m.end()) {
+		cout << "not found" << endl;
+	} else {
+		cout << "found:" << itr->second << endl;
+	}
+}
+
 int main() {
 
 	map<int, string> m;
@@ -19,8 +31,24 @@ int main() {
 	m[2] = "two";
 	m[3] = "three";
 	m[4] = "four";
+	m[2] = "TWO!";	//operator[] returns string& here since value exists
 
-	cout << m[2] << endl;
+	//Each element of map is of type pair<const K, V>
+	//here it is pair<const int, string>
+
+	//map<int, string>::iterator, this is verbose
+	// this loops is very similar to that used for vectors and other stls
+	for (auto itr = m.cbegin(); itr != m.cend(); ++itr) { 
+		//Since we are using map we get elements in the sorted order of keys
+		//Default behaviour is less than '<'
+		cout <<  "(" << itr->first << ", " << itr->second << ")" << endl;
+	}
+
+	foo(m);
+
+	//map is implemented with a balanced binary tree
+	//insert O(log N)
+	//erase O(log N)
 
 	m.clear();
 }
